4c745b697dca0bc8043c23b6600481d0bca5970f
Timoha <tim.valiev.00@mail.ru>
2021-03-06 01:16:12 +0500

    добавлены методы для работы с бд

--- Test Task for Веселый водовоз/Data/SQL.cs
+++ Test Task for Веселый водовоз/Data/SQL.cs
@@ -8,17 +8,78 @@ namespace TestTask.Data
     {
         public List<Employee> SelectEmployee()
         {
-            //IQueryable employees;
             using (var context = new MyDbContext())
             {
-                //var employees = from e in context.Employees
-                //                select e;
                 var employees = (from e in context.Employees
-                                 join d in context.Departments on e.Id equals d.DepartmentDirectorId
                                  select e).ToList<Employee>();
                 return employees;
             }
+        }
 
+        public bool UpdateEmployee(List<Employee> employees)
+        {
+            using(var context = new MyDbContext())
+            {
+                if (!Equals(context.Employees, employees))
+                {
+                    //я не придумал ничего лучше)) уж извиняйте)
+                    context.Employees = null;
+                    context.Employees.AddRange(employees);
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        public List<Department> SelectDepartment()
+        {
+            using (var context = new MyDbContext())
+            {
+                var departments = (from e in context.Departments
+                                 select e).ToList<Department>();
+                return departments;
+            }
+        }
+
+        public bool UpdateDepartment(List<Department> departments)
+        {
+            using (var context = new MyDbContext())
+            {
+                if (!Equals(context.Departments, departments))
+                {
+                    //я не придумал ничего лучше)) уж извиняйте)
+                    context.Departments = null;
+                    context.Departments.AddRange(departments);
+                    return true;
+                }
+                return false;
+            }
+        }
+
+
+        public List<Order> SelectOrder()
+        {
+            using (var context = new MyDbContext())
+            {
+                var orders = (from e in context.Orders
+                                 select e).ToList<Order>();
+                return orders;
+            }
+        }
+
+        public bool UpdateOrder(List<Order> orders)
+        {
+            using (var context = new MyDbContext())
+            {
+                if (!Equals(context.Orders, orders))
+                {
+                    //я не придумал ничего лучше)) уж извиняйте)
+                    context.Orders = null;
+                    context.Orders.AddRange(orders);
+                    return true;
+                }
+                return false;
+            }
         }
     }
 }
